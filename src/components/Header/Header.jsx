0cd848f40974d1125ea6b40607c5a796a88bd38c
camping <simsim2009@naver.com>
2024-07-03 11:43:03 +0900

    todo-list 첫번째

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { IoMoon, IoSunnyOutline } from "react-icons/io5";
+import { useDarkMode } from '../../context/DarkModeContext';
+
+function Header({filters,filter,onFilterChange}) {//header안의 중괄호 --> 구조분해 값을 받아야함
+  const {darkMode,toggleDarkMode}=useDarkMode();
+  return (
+    <header className='header'>
+      <button className='toggle'onClick={toggleDarkMode}>
+        {!darkMode && <IoMoon />} 
+        {!darkMode && <IoSunnyOutline />}
+      </button>
+      <ul className='filters'>
+        {
+          filters.map((value,index)=>{
+            return ( //괄호 --> 값
+              <li key={index}>
+                <button
+                  className="filter"
+                  onClick={() => {
+                    onFilterChange(value);
+                  }}
+                >
+                  {value}
+                </button>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </header>
+  )
+}
+
+export default Header
+
+//수출--> 자식
+//수입--> 부모
